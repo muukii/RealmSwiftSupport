4bb41957c6105a4d6324f2d90dde43e7df5d2090
Muukii <muukii.app@gmail.com>
2020-06-11 00:50:59 +0900

    Initial Commit

---
+++ RealmSwiftSupport/RealmSwiftSupport.h
@@ -0,0 +1,19 @@
+//
+//  RealmSwiftSupport.h
+//  RealmSwiftSupport
+//
+//  Created by muukii on 2020/06/11.
+//  Copyright © 2020 muukii. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RealmSwiftSupport.
+FOUNDATION_EXPORT double RealmSwiftSupportVersionNumber;
+
+//! Project version string for RealmSwiftSupport.
+FOUNDATION_EXPORT const unsigned char RealmSwiftSupportVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RealmSwiftSupport/PublicHeader.h>
+
+
